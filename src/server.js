865381b5a6323dfd6d22569cb3a52e150d4e85ed
Gabriel Valladares <gabrielvalladares1@gmail.com>
2020-12-08 17:09:16 -0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+
+const app = express(); 
+
+app.use(express.static(path.join(__dirname, '/build')))
+app.use(bodyParser.json());
+
+const withDB = async (operations) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+        const db = client.db('my-portfolio');
+
+        await operations(db);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json( { message: 'Error connection to db', error });
+    }
+}
+
+app.get('/api/projects/:name', async (req, res) => {
+    withDB(async (db) => {
+        const projectName = req.params.name;
+
+        const projectInfo = await db.collection('projects').findOne({ name: projectName });
+        res.status(200).json(projectInfo);
+    }, res);
+})
+
+app.post('/api/projects/:name/upvote', async (req, res) => {
+    withDB(async (db) => {
+        const projectName = req.params.name;
+
+        const projectInfo = await db.collection('projects').findOne({ name: projectName });
+        await db.collection('projects').updateOne({ name: projectName}, {
+            '$set': {
+                upvotes: projectInfo.upvotes + 1,
+            },
+        });
+
+        const updatedProjectInfo = await db.collection('projects').findOne({ name: projectName });
+        res.status(200).json(updatedProjectInfo);
+    }, res);
+});
+
+app.post('/api/projects/:name/add-comment', (req, res) => {
+    const { username, text} = req.body; 
+    const projectName = req.params.name;
+
+    withDB(async (db) => {
+        const projectInfo = await db.collection('projects').findOne({ name: projectName });
+        await db.collection('projects').updateOne({ name: projectName }, {
+            '$set': {
+                comments: projectInfo.comments.concat({ username, text }),
+            },
+        })
+        const updatedProjectInfo = await db.collection('projects').findOne({ name: projectName });
+        res.status(200).json(updatedProjectInfo)
+    }, res);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+app.listen(8000, () => console.log('Listening on port 8000'))
